b96d8b0f6f85638ca1e54dd60e4e2eca539bc56d
Apecker <lol.louis14@hotmail.fr>
2017-07-13 12:36:50 +0200

    changement complet du jeu

--- main.cpp
+++ main.cpp
@@ -1,142 +1,69 @@
-#include <stdio.h>
-
+/*
+main.c
+------
+ 
+Par apecker
+ 
+Rôle : menu du jeu. Permet de choisir entre l'éditeur et le jeu lui-même.
+*/
+ 
 #include <stdlib.h>
-
-
+#include <stdio.h>
 #include <SDL/SDL.h>
-
 #include <SDL/SDL_image.h>
-
-
-#include "jeu.h"
-
+ 
 #include "constantes.h"
-
-
-int main ()
-
+#include "jeu.h"
+ 
+ 
+ 
+int main(int argc, char *argv[])
 {
-
-    //création des surfaces et des noms de positions
-
-    SDL_Surface *ecran = NULL, *menu = NULL, *debut = NULL;
-
+    SDL_Surface *ecran = NULL, *menu = NULL;
     SDL_Rect positionMenu;
-
-
-
-
-    // positionnement des images
-
-    // menu
-
-    positionMenu.x = 0;
-
-    positionMenu.y = 0;
-
-
-
-
-
-    // création des evenements
-
-
-    // principal :
-
     SDL_Event event;
-
-
-
-
-    // integer pour la boucle infinie
+ 
     int continuer = 1;
-
-
-
-
-    // initialize SDL video
-
+ 
     SDL_Init(SDL_INIT_VIDEO);
-
-
-
-
-
-
-    // icone (a mettre après)
-
-
-
-
-
-
-
-    // create a new window
-
-    SDL_Surface* screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-
-
-
-
-
-    // load an image
-
-    menu = IMG_Load("menu.png");
-
-    SDL_BlitSurface(menu, NULL, ecran, &positionMenu);
-
-
-    SDL_Flip(ecran);
-
-
-
-
-
-    // program main loop
-
+ 
+    SDL_WM_SetIcon(IMG_Load("caisse.jpg"), NULL); // L'icône doit être chargée avant SDL_SetVideoMode
+    ecran = SDL_SetVideoMode(LARGEUR_FENETRE, HAUTEUR_FENETRE, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    SDL_WM_SetCaption("Mario Sokoban", NULL);
+ 
+    menu = IMG_Load("menu.jpg");
+    positionMenu.x = 0;
+    positionMenu.y = 0;
+ 
     while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
         {
-            SDL_WaitEvent(&event);
-            switch(event.type)
-            {
-                case SDL_QUIT:
-                    continuer = 0;
-                    break;
-                case SDL_KEYDOWN:
-                    switch(event.key.keysym.sym)
-                    {
-                        case SDLK_ESCAPE: // Veut arrêter le jeu
-                            continuer = 0;
-                            break;
-                        case SDLK_KP1: // Demande à jouer
-                            jouer(ecran);
-                            break;
-                    }
-                    break;
-            }
-
-            // Effacement de l'écran
-            SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 0, 0, 0));
-            SDL_BlitSurface(menu, NULL, ecran, &positionMenu);
-            SDL_Flip(ecran);
+            case SDL_QUIT:
+                continuer = 0;
+                break;
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE: // Veut arrêter le jeu
+                        continuer = 0;
+                        break;
+                    case SDLK_KP1: // Demande à jouer
+                        jouer(ecran);
+                        break;
+                }
+                break;
         }
-
-
-    // free loaded bitmap
-
-    SDL_FreeSurface(debut);
-
-
-	// etteinte de la SDL
+ 
+        // Effacement de l'écran
+        SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 0, 0, 0));
+        SDL_BlitSurface(menu, NULL, ecran, &positionMenu);
+        SDL_Flip(ecran);
+    }
+ 
+    SDL_FreeSurface(menu);
     SDL_Quit();
-
-
-
-
-    // all is well ;)
-
-    printf("Exited cleanly\n");
-
-
-    return 0;
+ 
+    return EXIT_SUCCESS;
 }
